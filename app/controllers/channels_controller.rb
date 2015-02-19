d1ccb1035507693c41911e8bf04e803d682f7aaa
Matthew Gates <matthewg42@gmail.com>
2015-02-19 11:23:33 +0000

    upload_csv API call implemented

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,7 +1,7 @@
 class ChannelsController < ApplicationController
   include ChannelsHelper, ApiKeys
   before_filter :authenticate_via_api_key!, :only => [:index]
-  before_filter :require_user, :except => [:realtime, :realtime_update, :show, :post_data, :social_show, :social_feed, :public]
+  before_filter :require_user, :except => [:realtime, :realtime_update, :show, :post_data, :post_csv, :social_show, :social_feed, :public]
   before_filter :set_channels_menu
   layout 'application', :except => [:social_show, :social_feed]
   protect_from_forgery :except => [:realtime, :realtime_update, :post_data, :create, :destroy, :clear]
